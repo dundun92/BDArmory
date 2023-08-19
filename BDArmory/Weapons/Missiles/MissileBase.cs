5da7260671aa1bb3d8f4381528f2623e1d4b6e5e
dundun92 <30281012+dundun92@users.noreply.github.com>
2023-08-19 01:54:52 -0500

    FJRT3 missile DLZ fix

--- BDArmory/Weapons/Missiles/MissileBase.cs
+++ BDArmory/Weapons/Missiles/MissileBase.cs
@@ -73,6 +73,10 @@ namespace BDArmory.Weapons.Missiles
         [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "#LOC_BDArmory_MinStaticLaunchRange"), UI_FloatRange(minValue = 10f, maxValue = 4000f, stepIncrement = 100f, scene = UI_Scene.Editor, affectSymCounterparts = UI_Scene.All)]//Min Static Launch Range
         public float minStaticLaunchRange = 10;
 
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Clamped DLZ"), // if true, DLZ is clamped as per "old" behavior
+            UI_Toggle(disabledText = "#LOC_BDArmory_UnclampTuning_enabledText", enabledText = "#LOC_BDArmory_UnclampTuning_disabledText", scene = UI_Scene.All, affectSymCounterparts = UI_Scene.All)]
+        public bool clampedDLZ = true;
+
         public float StandOffDistance = -1;
 
         [KSPField]
